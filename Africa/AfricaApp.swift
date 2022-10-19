e3e399995cea20f311f8177b068011f12b98a8bc
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-19 14:55:25 +0000

    create africa project and add assets

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Mary Moreira on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
