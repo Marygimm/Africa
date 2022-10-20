900329174f56562cac85ff2865a0e7c58ba43802
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-20 14:23:21 +0000

    add a grid to the gallery with slider dynamic changing the columns

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,12 +9,68 @@ import SwiftUI
 
 struct GalleryView: View {
     //MARK: - Properties
+    @State private var selectedAnimal: String = "lion"
     
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    //Simple grid definition
+   // let gridLayout: [GridItem] = [
+   //     GridItem(.flexible()),
+   //     GridItem(.flexible()),
+   //     GridItem(.flexible())
+   // ]
+    
+    //Efficient grid definition
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    //dinamic grid layout
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
     
     //MARK: - Body
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Galery")
+            VStack(alignment: .center, spacing: 30) {
+                //MARK: - Image
+                
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                //MARK: - Slider
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn) { newValue in
+                        gridSwitch()
+                    }
+                
+                //MARK: - GRID
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    }//: Loop
+                } // Grid
+                .onAppear {
+                    gridSwitch()
+                }
+            } //: VStack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: Scroll
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
