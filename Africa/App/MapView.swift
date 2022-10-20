fc3344db0e509e426c5b0d6ed34669a9f893fe90
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-20 12:34:51 +0000

    create map model and map view with custom annotations

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,10 +6,43 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    //MARK: - Properties
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.00286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    
+        
+    //MARK: - Body
     var body: some View {
-        Text("Map")
+        //MARK: - No1 Basic Map
+//        Map(coordinateRegion: $region)
+        
+    //MARK: No2 ADVANCED MAP
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+            // (A) PIN: OLD STYLE AND STATIC
+//            MapPin(coordinate: item.location, tint: .accentColor)
+            
+            // (B) MARKER: NEW STYLE STATIC
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // (C) CUSTOM BASIC ANNOTATION (it could be interactive)
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+            } //: Annotation
+        })
     }
 }
 
